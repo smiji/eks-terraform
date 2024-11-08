69e54dde2c54a785f11909c7b10c8df9a9ecfbeb
Arun <arun@test.com>
2024-11-08 23:23:45 +0000

    Terraform for EKS

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+#EKS Cluster 
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name                 = "eks-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["${var.region}a", "${var.region}b"]
+  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets      = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "18.0.0"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.24"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t2.micro"
+
+      tags = {
+        Name = "eks-node"
+      }
+    }
+  }
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_id
+}
+
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
+
+output "aws_region" {
+  value = var.region
+}
