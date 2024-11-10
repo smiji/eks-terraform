f6fa0d9b5e6e9f53d55c412c8f9caee26a6e363a
Smiji John <johnsmiji861@gmail.com>
2024-11-10 20:32:38 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.28.0"
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.3.2"
   subnet_ids         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
