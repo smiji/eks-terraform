acd0569e62aa7bdf8db9f02d6e3329850f9c3ea7
Arun <arun@test.com>
2024-11-11 13:37:23 +0000

    remove data and add module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,8 @@
-#Kubernetes Provider 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = module.eks.cluster_id.endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_id.certificate_authority.0.data)
+  token                  = module.eks.cluster_id.token
   version                = ">= 2.20.0"
 }
 
