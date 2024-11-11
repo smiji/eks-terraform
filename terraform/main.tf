0011599f56926efcf366ebd5e9fe67158661ed75
Arun <arun@test.com>
2024-11-11 13:32:55 +0000

    ks8 config

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "~> 1.3"
+  version                = ">= 2.20.0"
 }
 
 #EKS Cluster 
